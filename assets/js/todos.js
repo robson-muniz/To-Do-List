f02143572979e7283bfe8577600edadcb634baea
robson-muniz <robson_muniz@yahoo.com>
2019-09-11 21:27:02 -0300

    Todo list: creating todos.

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+// Check off specifics todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete todos
+$("ul").on("click", "span",function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //Grabing new todo text from input
+        var todoText = ($(this).val());
+        $(this).val("");
+        //Create a new li and add it to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }    
+});
